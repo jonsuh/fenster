e124225f1f3b127347ed8112441f13d0e9ede3f0
Jonathan Suh <hello@jonsuh.com>
2015-02-26 15:46:11 -0600

    Change Grunt includePaths to reflect new Bourbon.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
     sass: {
       options: {
         // includePaths: [
-        //   'bower_components/bourbon/dist',
+        //   'bower_components/bourbon/app/assets/stylesheets',
         // ],
         // sourceMap: true,
       },
