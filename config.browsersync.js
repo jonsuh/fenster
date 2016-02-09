b5092479de45779def927b24787cf77590af916f
Jonathan Suh <hello@jonsuh.com>
2016-02-09 15:51:16 -0600

    npm scripts with Browsersync and PostCSS config files.

---
+++ config.browsersync.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "server": {
+    baseDir: "./"
+  },
+  "files": [
+    "assets/css/**/*.css"
+  ],
+  "port": 3000,
+  "proxy": false,
+  "open": false,
+  "notify": false,
+};
