39293a461820fbce3530a1a4c1f5fd1f2da37c87
Jonathan Suh <hello@jonsuh.com>
2016-12-20 17:04:15 -0600

    fester.js for replacing package.json based on type.

---
+++ bin/fester.js
@@ -0,0 +1,65 @@
+#! /usr/bin/env node
+const fs = require("fs");
+const path = require("path");
+
+let packageFile = require("../package.json");
+
+const typeDefault = "yarn";
+let type          = typeDefault;
+
+
+// Try to get value of --type flag
+const args = process.argv.slice(2);
+const flag = args[0];
+
+if (flag === "-t" || flag === "--type") {
+  if (args[1] !== undefined) {
+    type = args[1].toLowerCase();
+
+    if (type === "npm" || type === "gulp" || type === "grunt") {
+      switch(type) {
+        case "gulp":
+          // copy gulpfile.js
+          break;
+        case "grunt":
+          // copy Gruntfile.js
+          break;
+      }
+    }
+    else {
+      type = typeDefault;
+    }
+  }
+}
+
+
+// Read the contents of package.type.json
+let typeFile;
+
+try {
+  typeFile = fs.readFileSync("bin/src/package." + type + ".json", "utf8");
+}
+catch(error) {
+  console.log(error);
+  process.exit(1);
+}
+
+typeFile = JSON.parse(typeFile);
+
+
+// Replace the devDependencies and scripts contents of package.json with package.type.json
+packageFile.devDependencies = typeFile.devDependencies;
+packageFile.scripts         = typeFile.scripts;
+
+
+// Write the new contents to package.json
+try {
+  fs.writeFile("package.json", JSON.stringify(packageFile, null, 2));
+}
+catch(error) {
+  if (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
